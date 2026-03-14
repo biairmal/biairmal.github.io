0a228a891ab3ec71350bb3bffcc1989bb3431c88
Bandana Irmal A <bandana314i@gmail.com>
2026-03-14 21:21:29 +0700

    feat: homepage content

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,64 @@
-import Image from "next/image";
+// import Image from "next/image";
+
+const careerStartDate = new Date(2021, 6, 1);
 
 export default function Home() {
+  function getAge(date: Date) {
+    const today = new Date();
+    let age = today.getFullYear() - date.getFullYear();
+    const m = today.getMonth() - date.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < date.getDate())) {
+      age--;
+    }
+    return age;
+  }
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+      <div className="flex min-h-screen w-full max-w-7xl flex-col items-center justify-between py-16 px-6 md:py-32 md:px-16 bg-zinc-50 dark:bg-black sm:items-start">
+        <div className="flex flex-col space-y-8 text-lg max-w-xl">
+          <h1 className="text-5xl font-medium pb-8">Bandana Irmal Abdillah</h1>
+          <p className="">
+            Hi there! I’m an experienced software engineer based in Jakarta,
+            Indonesia, with over {getAge(careerStartDate)} years of experience
+            building digital products. I primarily focus on backend development,
+            but I’m also comfortable working on frontend applications when
+            needed.
+          </p>
+          <p>
+            I currently work at a software house where I collaborate with
+            different clients and work with various technologies across multiple
+            projects. Before starting my career as a software engineer, I earned
+            a Bachelor’s degree in Computer Science from{" "}
+            <a>Universitas Padjadjaran</a>. I also occasionally take on
+            freelance projects to work on new challenges and continue improving
+            my skills.
+          </p>
+          <p>
+            Over the years, I’ve developed experience with various technologies
+            and tools while building and maintaining software systems. Some of
+            the technologies I’ve worked with include:
+          </p>
+          <ul>
+            <li>
+              <b>Backend:</b> .Net Core, Spring Boot (Java), Golang, Node.js
+            </li>
+            <li>
+              <b>Frontend:</b> React.js, Next.js, Blazor, TailwindCSS
+            </li>
+            <li>
+              <b>Database:</b> PostgreSQL, SQLServer, Oracle, CosmosDB, Redis
+            </li>
+            <li>
+              <b>DevOps:</b> Docker, CI/CD Pipelines, Azure, AWS
+            </li>
+          </ul>
+          <p>
+            Outside of programming, I enjoy exploring new foods, playing video
+            games, and playing my guitar. Feel free to connect with me!
           </p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      </div>
     </div>
   );
 }
